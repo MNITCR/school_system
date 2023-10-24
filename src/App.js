e1b3681ae03c1dad655b770e08481e4eb59a3742
MNITCR <108656979+MNITCR@users.noreply.github.com>
2023-10-24 08:05:58 +0700

    cm

---
+++ src/App.js
@@ -0,0 +1,73 @@
+import React, { useState } from 'react';
+import './App.css'; // Import the generated Tailwind CSS file
+
+function App() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [emailError, setEmailError] = useState('');
+  const [passwordError, setPasswordError] = useState('');
+
+  const handleLogin = () => {
+    // Reset previous errors
+    setEmailError('');
+    setPasswordError('');
+
+    // Validate email
+    if (!email) {
+      setEmailError('Please enter your email.');
+      return;
+    }
+
+    // Validate password
+    if (!password) {
+      setPasswordError('Please enter your password.');
+      return;
+    }
+
+    // Implement your login logic here
+    console.log('Email:', email);
+    console.log('Password:', password);
+  };
+
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-200">
+      <div className="bg-white p-8 rounded shadow-md w-96">
+        <h2 className="text-2xl font-semibold mb-4">Login</h2>
+        <div className="mb-4">
+          <label htmlFor="email" className="block text-sm font-medium text-gray-600">
+            Email
+          </label>
+          <input
+            type="email"
+            id="email"
+            className={`mt-1 p-2 w-full border rounded-md ${emailError && 'border-red-500'}`}
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          {emailError && <p className="text-red-500 text-xs mt-1">{emailError}</p>}
+        </div>
+        <div className="mb-4">
+          <label htmlFor="password" className="block text-sm font-medium text-gray-600" >
+            Password
+          </label>
+          <input
+            type="password"
+            id="password"
+            className={`mt-1 p-2 w-full border rounded-md ${passwordError && 'border-red-500'}`}
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          {passwordError && <p className="text-red-500 text-xs mt-1">{passwordError}</p>}
+        </div>
+        <button
+          className="bg-blue-500 text-white p-2 rounded-md w-full"
+          onClick={handleLogin}
+        >
+          Login
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
